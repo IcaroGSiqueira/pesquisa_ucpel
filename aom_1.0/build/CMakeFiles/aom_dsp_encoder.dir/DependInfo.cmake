fbdafd1aa75a8d921f471c9c3259e0bc985c983d
icaro siqueira <icaro.siqueira@sou.ucpel.edu.br>
2019-05-16 17:37:49 -0300

    commit

---
+++ aom_1.0/build/CMakeFiles/aom_dsp_encoder.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/binary_codes_writer.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/binary_codes_writer.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/daalaboolwriter.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/daalaboolwriter.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/entenc.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/entenc.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/fwd_txfm.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/fwd_txfm.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/grain_table.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/grain_table.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/noise_model.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/noise_model.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/noise_util.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/noise_util.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/psnr.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/psnr.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/quantize.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/quantize.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/sad.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/sad.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/sad_av1.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/sad_av1.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/sse.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/sse.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/sum_squares.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/sum_squares.c.o"
+  "/home/icaro/pesquisa_ucpel/aom/aom_dsp/variance.c" "/home/icaro/pesquisa_ucpel/aom/build/CMakeFiles/aom_dsp_encoder.dir/aom_dsp/variance.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../"
+  "."
+  "../apps"
+  "../common"
+  "../examples"
+  "../stats"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

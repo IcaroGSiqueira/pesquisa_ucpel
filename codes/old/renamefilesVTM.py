49902d31293a22e30c0940b457b158bf2d094499
icaro siqueira <icaro.siqueira@sou.ucpel.edu.br>
2019-05-29 14:03:27 -0300

    commit

--- codes/old/renamefilesVTM.py
+++ codes/old/renamefilesVTM.py
@@ -3,5 +3,5 @@ files = os.listdir("/home/grellert/testesVVC/out/")
 for file in files:
 	str1,str2 = file.split(".yuv")
 	str0 = str1+str2
-	mv = "mv "+"/home/grellert/testesVVC/out/"+file+ "/home/grellert/testesVVC/out/"+str0
+	mv = "mv "+"/home/grellert/testesVVC/out/"+file+ " /home/grellert/testesVVC/out/"+str0
 	os.system(mv)

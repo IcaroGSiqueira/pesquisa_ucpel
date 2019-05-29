f5cbd477a01ab569e6474c6242ec83a368b62021
icaro siqueira <icaro.siqueira@sou.ucpel.edu.br>
2019-05-29 14:03:27 -0300

    commit

--- codes/old/renamefiles.py
+++ codes/old/renamefiles.py
@@ -3,5 +3,5 @@ files = os.listdir("/home/grellert/testesHEVC/out/")
 for file in files:
 	str1,str2 = file.split(".yuv")
 	str0 = str1+str2
-	mv = "mv "+"/home/grellert/testesHEVC/out/"+file+ "/home/grellert/testesHEVC/out/"+str0
+	mv = "mv "+"/home/grellert/testesHEVC/out/"+file+ " /home/grellert/testesHEVC/out/"+str0
 	os.system(mv)

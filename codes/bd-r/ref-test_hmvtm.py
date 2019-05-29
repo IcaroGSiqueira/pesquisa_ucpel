8933307ced2bc4d2a6cbe290e64213dbbcf944f0
icaro siqueira <icaro.siqueira@sou.ucpel.edu.br>
2019-05-29 15:03:06 -0300

    commit

--- codes/bd-r/ref-test_hmvtm.py
+++ codes/bd-r/ref-test_hmvtm.py
@@ -1,8 +1,8 @@
 from bjontegaardhmvtm import bdbr,bdpsnr,plotRDCurves
 
-csv_test = open('/home/grellert/testesHEVC/hm-noSIMD.csv','rb')
-csv_ref = open('/home/grellert/testesVVC/vtm-noSIMD.csv','rb')
-out = open("/home/grellert/pesquisa_ucpel/bd-rate/hm-vtm_noSIMD.csv","w")
+csv_test = open('/home/icaro/testesHEVC/hm-noSIMD.csv','rb')
+csv_ref = open('/home/icaro/testesVVC/vtm-noSIMD.csv','rb')
+out = open("/home/icaro/pesquisa_ucpel/bd-rate/hm-vtm_noSIMD.csv","w")
 
 lines = csv_ref.readlines()
 tlines = csv_test.readlines()
@@ -12,19 +12,31 @@ for x in xrange(0,tam):
 	if "qp22" in lines[x]:
 		print lines[x]
 		nome22,y22,u22,v22,yuv22,b22,t22 = lines[x].split(",")
-		n,n1,n2,n3,n4,n5 = nome22.split("_")
+		if "crop" in nome22:
+			n,n1,n2,n3,n4,n5,n6,n7 = nome22.split("_")
+		else:
+			n,n1,n2,n3,n4,n5,n6 = nome22.split("_")
 		y22,u22,v22,yuv22,b22,t22 = map(float,[y22,u22,v22,yuv22,b22,t22])
 	if "qp27" in lines[x]:
 		nome27,y27,u27,v27,yuv27,b27,t27 = lines[x].split(",")
-		n,n1,n2,n3,n4,n5 = nome27.split("_")
+		if "crop" in nome27:
+			n,n1,n2,n3,n4,n5,n6,n7 = nome27.split("_")
+		else:
+			n,n1,n2,n3,n4,n5,n6 = nome27.split("_")
 		y27,u27,v27,yuv27,b27,t27 = map(float,[y27,u27,v27,yuv27,b27,t27])
 	if "qp32" in lines[x]:
 		nome32,y32,u32,v32,yuv32,b32,t32 = lines[x].split(",")
-		n,n1,n2,n3,n4,n5 = nome32.split("_")
+		if "crop" in nome32:
+			n,n1,n2,n3,n4,n5,n6,n7 = nome32.split("_")
+		else:
+			n,n1,n2,n3,n4,n5,n6 = nome32.split("_")
 		y32,u32,v32,yuv32,b32,t32 = map(float,[y32,u32,v32,yuv32,b32,t32])
 	if "qp37" in lines[x]:
 		nome37,y37,u37,v37,yuv37,b37,t37 = lines[x].split(",")
-		n,n1,n2,n3,n4,n5 = nome37.split("_")
+		if "crop" in nome37:
+			n,n1,n2,n3,n4,n5,n6,n7 = nome37.split("_")
+		else:
+			n,n1,n2,n3,n4,n5,n6 = nome37.split("_")
 		y37,u37,v37,yuv37,b37,t37 = map(float,[y37,u37,v37,yuv37,b37,t37])
 
 	if "qp22" in tlines[x]:
@@ -49,7 +61,11 @@ for x in xrange(0,tam):
 		tref = (t22+t37+t27+t32)/4
 		dt=tref/ttest
 
+		nome=n+"_"+n1+"_"+n2
+
 		bdb = bdbr(ref,test,1)
+		bdbyuv = bdbr(ref,test,4)
 		bdp = bdpsnr(ref,test,1) 
+		bdpyuv = bdpsnr(ref,test,4) 
 		plotRDCurves(ref,test,1,"./curvas/Curva%s.pdf"%n,n)
-		print >> out,n,bdb,bdp,dt
+		print >> out,nome,bdb,bdbyuv,bdp,bdpyuv,dt

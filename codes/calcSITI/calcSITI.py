8242d6e289f6dc2e60fcbc6f1fb7d515830e41cb
icaro siqueira <icaro.siqueira@sou.ucpel.edu.br>
2019-06-06 17:24:13 -0300

    commit

--- codes/calcSITI/calcSITI.py
+++ codes/calcSITI/calcSITI.py
@@ -70,7 +70,7 @@ def getYFrame(video,w,h):
 #yuvs = {"BasketballDrill_832x480_50.yuv","BasketballDrive_1920x1024_50.yuv","BasketballDrive_1920x1080_50.yuv","BasketballPass_416x240_50.yuv","Kimono_1920x1080_24.yuv","BlowingBubbles_416x240_50.yuv","ParkScene_1920x1080_24.yuv","BQMall_832x480_60.yuv","BQSquare_416x240_60.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","PartyScene_832x480_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","Traffic_2560x1600_30_crop.yuv","SlideEditing_1280x720_30.yuv","Tennis_1920x1080_24.yuv"}
 
 
-videos = os.listdir("/workareas/share/video_sequences")
+videos = os.listdir("/workareas/share/video_sequences/")
 
 for v in videos:
 #	if v not in yuvs:
@@ -84,7 +84,7 @@ for v in videos:
 		continue
 	video = open('/workareas/share/video_sequences/' + v,'rb')
 	w = int((v.split('_')[1]).split('x')[0])
-	h =  int((v.split('_')[1]).split('x')[1])
+	h =  int((v.split('_')[1]).split('x')[1].split('.')[0])
 	outFile = open(v.split('_')[0]+'.csv','w')
 	frame = getYFrame(video,w,h)
 	prevFrame = frame

6e283a7dee05156ae783f47f3a42e4ef2da18608
icaro siqueira <icaro.siqueira@sou.ucpel.edu.br>
2019-06-06 14:10:24 -0300

    commit

--- run-sh/run_vtm_server.py
+++ run-sh/run_vtm_server.py
@@ -1,11 +1,16 @@
 import os
-yuvs = {"BasketballDrill_832x480_50.yuv","BasketballDrive_1920x1024_50.yuv","BasketballDrive_1920x1080_50.yuv","BasketballPass_416x240_50.yuv","Kimono_1920x1080_24.yuv","BlowingBubbles_416x240_50.yuv","ParkScene_1920x1080_24.yuv","BQMall_832x480_60.yuv","BQSquare_416x240_60.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","PartyScene_832x480_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","Traffic_2560x1600_30_crop.yuv"}
 
-f=60
+f=10
+simd=0
+
+#yuvs = {"BasketballDrill_832x480_50.yuv","BasketballDrive_1920x1024_50.yuv","BasketballDrive_1920x1080_50.yuv","BasketballPass_416x240_50.yuv","Kimono_1920x1080_24.yuv","BlowingBubbles_416x240_50.yuv","ParkScene_1920x1080_24.yuv","BQMall_832x480_60.yuv","BQSquare_416x240_60.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","PartyScene_832x480_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","Traffic_2560x1600_30_crop.yuv","SlideEditing_1280x720_30.yuv","Tennis_1920x1080_24.yuv"}
+
+yuvs = {"Traffic_2560x1600_30_crop.yuv","SlideEditing_1280x720_30.yuv","Tennis_1920x1080_24.yuv"}
+
 homepath = "/home/grellert"
 yuvpath = "/workareas/share/video_sequences"
-#yuvpath = "/home/icaro/origCfP"
 
+#yuvs = os.listdir("%s" %yuvpath)
 file = open("run_vtm_server.sh","w")
 for yuv in yuvs:
 	for qp in [37,32,27,22]:
@@ -20,6 +25,9 @@ for yuv in yuvs:
 			nome = vid+"_"+pix+"_"+fr+"_qp%s_"%(qp)+y
 		w,h = pix.split("x")
 		#print w,h,f,fr,qp
-		linha = "{ time %s/pesquisa_ucpel/VTM_5.0_noSIMD/bin/EncoderAppStatic -c %s/pesquisa_ucpel/VTM_5.0_noSIMD/cfg/encoder_randomaccess_vtm.cfg --InputFile=%s/%s --SourceHeight=%s --SourceWidth=%s -f %s -fr %s -q %s --BitstreamFile=%s/testesVVC/bin/%s_%sfr_vtm.bin ; } &> %s/testesVVC/out/%s_qp%s_%sfr_vtm_out" %(homepath,homepath,yuvpath,yuv,h,w,f,fr,qp,homepath,nome,f,homepath,yuv,qp,f)
+		if simd == 1:
+			linha = "%s/vtm_5.0/bin/EncoderAppStatic -c %s/vtm_5.0/cfg/encoder_randomaccess_vtm.cfg --InputFile=%s/%s --SourceHeight=%s --SourceWidth=%s -f %s -fr %s -q %s --BitstreamFile=%s/testesVVC/bin/%s_vtmSIMD.bin  > %s/testesVVC/out/%s_qp%s_vtmSIMD_out" %(homepath,homepath,yuvpath,yuv,h,w,f,fr,qp,homepath,nome,homepath,yuv,qp)
+		else:
+			linha = "%s/VTM_5.0_noSIMD/bin/EncoderAppStatic -c %s/VTM_5.0_noSIMD/cfg/encoder_randomaccess_vtm.cfg --InputFile=%s/%s --SourceHeight=%s --SourceWidth=%s -f %s -fr %s -q %s --BitstreamFile=%s/testesVVC/bin/%s_vtmNoSIMD.bin  > %s/testesVVC/out/%s_qp%s_vtmNoSIMD_out" %(homepath,homepath,yuvpath,yuv,h,w,f,fr,qp,homepath,nome,homepath,yuv,qp)
 		print >> file, linha
 		file.close

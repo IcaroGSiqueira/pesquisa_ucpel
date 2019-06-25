cf63d774b8e684176ea8cae0d60c942c50f50927
icaro siqueira <icaro.siqueira@sou.ucpel.edu.br>
2019-06-25 16:39:58 -0300

    commit

---
+++ VTM_5.0/source/App/EncoderApp/CMakeFiles/EncoderApp.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/icaro/pesquisa_ucpel/VTM_5.0/source/App/EncoderApp/EncApp.cpp" "/home/icaro/pesquisa_ucpel/VTM_5.0/source/App/EncoderApp/CMakeFiles/EncoderApp.dir/EncApp.cpp.o"
+  "/home/icaro/pesquisa_ucpel/VTM_5.0/source/App/EncoderApp/EncAppCfg.cpp" "/home/icaro/pesquisa_ucpel/VTM_5.0/source/App/EncoderApp/CMakeFiles/EncoderApp.dir/EncAppCfg.cpp.o"
+  "/home/icaro/pesquisa_ucpel/VTM_5.0/source/App/EncoderApp/encmain.cpp" "/home/icaro/pesquisa_ucpel/VTM_5.0/source/App/EncoderApp/CMakeFiles/EncoderApp.dir/encmain.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "source/App/EncoderApp"
+  "source/Lib/CommonLib/."
+  "source/Lib/CommonLib/.."
+  "source/Lib/CommonLib/./x86"
+  "source/Lib/CommonLib/../libmd5"
+  "source/Lib/EncoderLib/."
+  "source/Lib/DecoderLib/."
+  "source/Lib/Utilities/."
+  "source/Lib/Utilities/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/icaro/pesquisa_ucpel/VTM_5.0/source/Lib/CommonLib/CMakeFiles/CommonLib.dir/DependInfo.cmake"
+  "/home/icaro/pesquisa_ucpel/VTM_5.0/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/DependInfo.cmake"
+  "/home/icaro/pesquisa_ucpel/VTM_5.0/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/DependInfo.cmake"
+  "/home/icaro/pesquisa_ucpel/VTM_5.0/source/Lib/Utilities/CMakeFiles/Utilities.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

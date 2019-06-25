3268af6448cc0ecbb7049f394808e69fb4b2017f
icaro siqueira <icaro.siqueira@sou.ucpel.edu.br>
2019-06-25 15:38:49 -0300

    commit

--- codes/run-sh/py/run_vtm_gp.py
+++ codes/run-sh/py/run_vtm_gp.py
@@ -1,11 +1,8 @@
 import os
 
-f=5
+f=60
 
-
-#yuvs = {"BasketballDrill_832x480_50.yuv","BasketballDrive_1920x1024_50.yuv","BasketballDrive_1920x1080_50.yuv","BasketballPass_416x240_50.yuv","Kimono_1920x1080_24.yuv","BlowingBubbles_416x240_50.yuv","ParkScene_1920x1080_24.yuv","BQMall_832x480_60.yuv","BQSquare_416x240_60.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","PartyScene_832x480_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","Traffic_2560x1600_30_crop.yuv","SlideEditing_1280x720_30.yuv","Tennis_1920x1080_24.yuv"}
-
-yuvs = {"BQSquare_416x240_60.yuv"}
+yuvs = {"BasketballDrill_832x480_50.yuv","BasketballDrive_1920x1024_50.yuv","BasketballDrive_1920x1080_50.yuv","BasketballPass_416x240_50.yuv","Kimono_1920x1080_24.yuv","BlowingBubbles_416x240_50.yuv","ParkScene_1920x1080_24.yuv","BQMall_832x480_60.yuv","BQSquare_416x240_60.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","PartyScene_832x480_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","Traffic_2560x1600_30_crop.yuv","SlideEditing_1280x720_30.yuv","Tennis_1920x1080_24.yuv"}
 
 homepath = "/home/grellert"
 #homepath = "/home/icaro"
@@ -28,6 +25,7 @@ for yuv in yuvs:
 			continue
 		w,h = pix.split("x")
 		#print w,h,f,fr,qp
-		linha = "%s/pesquisa_ucpel/VTM_5.0/bin/EncoderAppStatic -c %s/pesquisa_ucpel/VTM_5.0/cfg/encoder_randomaccess_vtm.cfg --InputFile=%s/%s --SourceHeight=%s --SourceWidth=%s -f %s -fr %s -q %s --BitstreamFile=%s/testesVVC/gbin/%s.bin  > %s/testesVVC/gpout/%s_qp%s_vtm_out && gprof %s/pesquisa_ucpel/VTM_5.0/bin/EncoderAppStatic %s/pesquisa_ucpel/codes/run-sh/gmon.out > %s/testesHEVC/gprof_%s_qp%s_vtm.txt" %(homepath,homepath,yuvpath,yuv,h,w,f,fr,qp,homepath,nome,homepath,yuv,qp,homepath,homepath,homepath,yuv,qp)
+
+		linha = "%s/pesquisa_ucpel/VTM_5.0/bin/EncoderAppStatic_gp -c %s/pesquisa_ucpel/VTM_5.0/cfg/encoder_randomaccess_vtm.cfg --InputFile=%s/%s --SourceHeight=%s --SourceWidth=%s -f %s -fr %s -q %s --BitstreamFile=%s/testesVVC/bingp/%s.bin  > %s/testesVVC/outgp/%s_qp%s_vtm_out && gprof %s/pesquisa_ucpel/VTM_5.0/bin/EncoderAppStatic_gp %s/pesquisa_ucpel/codes/run-sh/gmon.out > %s/testesVVC/gprof/gprof_%s_qp%s_vtm.txt" %(homepath,homepath,yuvpath,yuv,h,w,f,fr,qp,homepath,nome,homepath,yuv,qp,homepath,homepath,homepath,yuv,qp)
 		print >> file, linha
 		file.close

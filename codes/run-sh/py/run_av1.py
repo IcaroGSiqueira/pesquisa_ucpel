c85025b2936091367a6f0f5e0c71b8f58898e9d0
IcaroGSiqueira <icaro.siqueira@sou.ucpel.edu.br>
2019-11-01 14:11:51 -0300

    commit

--- codes/run-sh/py/run_av1.py
+++ codes/run-sh/py/run_av1.py
@@ -1,15 +1,34 @@
 import os
 
-f=30
+f = 30
 
-server=0 # local = 0 ; servidor = 1
+gitpull = 1
 
-OPT=0 # optimizacoes ligadas = 1
-gprof=1
+server = 0 # local = 0 ; servidor = 1
+
+#qps = [37,32,27,22]
+
+qps = [27,22]
+
+OPT = 0 # optimizacoes ligadas = 1
+gprof = 1
+
+threads = 1 # numero de processos em parelelo
+
+gitpath = "pesquisa_ucpel"
 
 shpath = "pesquisa_ucpel/codes/run-sh/aom"
 filename = "run_av1.sh"
+
+yuvs = ["BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30.yuv"]
+
 #yuvs = os.listdir("/home/icaro/origCfP")
+
+#yuvs = ["BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30.yuv"] 														#ClasseD
+#yuvs = ["RaceHorses_832x480_30.yuv","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","PartyScene_832x480_50.yuv"] 																#ClasseC
+#yuvs = ["FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","SlideEditing_1280x720_30.yuv"] 																							#ClasseE e F
+#yuvs = ["Tennis_1920x1080_24.yuv","ParkScene_1920x1080_24.yuv","BasketballDrive_1920x1080_50.yuv","Kimono_1920x1080_24.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv"] 	#ClasseB
+#yuvs = ["PeopleOnStreet_2560x1600_30_crop.yuv","Traffic_2560x1600_30_crop.yuv"] 																										#ClasseA
 
 if server == 1:	
 	homepath = "/home/grellert"
@@ -17,14 +36,15 @@ if server == 1:
 	outpath = "pesquisa_ucpel/output_AV1/server"
 	binpath = "aom/build" #partindo da homepath
 
-	yuvs = ["BasketballDrill_832x480_50.yuv","BasketballDrive_1920x1080_50.yuv","BasketballPass_416x240_50.yuv","Kimono_1920x1080_24.yuv","BlowingBubbles_416x240_50.yuv","ParkScene_1920x1080_24.yuv","BQMall_832x480_60.yuv","BQSquare_416x240_60.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","PartyScene_832x480_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","Traffic_2560x1600_30_crop.yuv","SlideEditing_1280x720_30.yuv","Tennis_1920x1080_24.yuv"] 
+	yuvs = ["BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","PartyScene_832x480_50.yuv","FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","SlideEditing_1280x720_30.yuv","Tennis_1920x1080_24.yuv","ParkScene_1920x1080_24.yuv","BasketballDrive_1920x1080_50.yuv","Kimono_1920x1080_24.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","Traffic_2560x1600_30_crop.yuv"]
+
 else:
 	homepath = "/home/icaro"
 	yuvpath = "/home/icaro/origCfP"
 	outpath = "pesquisa_ucpel/output_AV1/local"
 	binpath = "aom/build" #partindo da homepath
 
-	yuvs = ["BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","PartyScene_832x480_50.yuv","ParkScene_1920x1080_24.yuv","BasketballDrive_1920x1080_50.yuv","Kimono_1920x1080_24.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","Traffic_2560x1600_30_crop.yuv"]
+	#yuvs = ["BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","PartyScene_832x480_50.yuv","ParkScene_1920x1080_24.yuv","BasketballDrive_1920x1080_50.yuv","Kimono_1920x1080_24.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","Traffic_2560x1600_30_crop.yuv"]
 
 if OPT == 1:
 	if gprof == 0:
@@ -62,8 +82,8 @@ if gprof == 1:
 	except:
 		pass
 
-for qp in [34,29,24,19]:
-	for yuv in yuvs:
+for yuv in yuvs:
+	for qp in qps:
 
 		if ".py" in yuv:
 			continue
@@ -79,20 +99,39 @@ for qp in [34,29,24,19]:
 			nome = vid+"_"+pix+"_"+fr
 
 		w,h = pix.split("x")
-		info = "%sqp_%sfframes_"%(qp+3,f) + inf
+		info = "%sqp_%sfframes_"%(qp,f) + inf
 
 		#qp = map(float,qp) !!--end-usage=q!!--AVALIAR
 
 		if gprof == 1:
-			linha = "%s/%s/%s --fps=%s/1 -w %s -h %s --min-q=%s --max-q=%s --limit=%s --rt -b 8 -o %s/%s/bin/%s_%s.bin %s/%s 2> %s/%s/out/%s_%s.txt"%(homepath,binpath,bina,fr,w,h,qp,qp+8,f,homepath,outpath,nome,info,yuvpath,yuv,homepath,outpath,nome,info)
+			linha = "%s/%s/%s --fps=%s/1 -w %s -h %s --min-q=%s --max-q=%s --limit=%s --rt -b 8 -o %s/%s/bin/%s_%s.bin %s/%s 2> %s/%s/out/%s_%s.txt"%(homepath,binpath,bina,fr,w,h,qp-3,qp+5,f,homepath,outpath,nome,info,yuvpath,yuv,homepath,outpath,nome,info)
 			linha2 = "mv %s/%s/gmon.out %s/%s/gmon/gmon_%s_%s.out"%(homepath,shpath,homepath,outpath,nome,info)
 			linha3 = "gprof %s/%s/%s %s/%s/gmon/gmon_%s_%s.out > %s/%s/gprof/%s_%s.txt"%(homepath,binpath,bina,homepath,outpath,nome,info,homepath,outpath,nome,info)
+			if gitpull == 1:
+				linha3 = linha3 + " && cd %s/%s && sh git_upl.sh"%(homepath,gitpath)
 			#linha4 =  "echo %s_%s DONE!"%(nome,info)
 			print >> file, linha + " && " + linha2 + " && " + linha3# + linha4
 		else:
-			linha = "{ time %s/%s/%s --fps=%s/1 -w %s -h %s --min-q=%s --max-q=%s --limit=%s --rt -b 8 -o %s/%s/bin/%s_%s.bin %s/%s ; } 2> %s/%s/out/%s_%s.txt"%(homepath,binpath,bina,fr,w,h,qp,qp+8,f,homepath,outpath,nome,info,yuvpath,yuv,homepath,outpath,nome,info)
+			linha = "{ time %s/%s/%s --fps=%s/1 -w %s -h %s --min-q=%s --max-q=%s --limit=%s --rt -b 8 -o %s/%s/bin/%s_%s.bin %s/%s ; } 2> %s/%s/out/%s_%s.txt"%(homepath,binpath,bina,fr,w,h,qp-3,qp+5,f,homepath,outpath,nome,info,yuvpath,yuv,homepath,outpath,nome,info)
+			if gitpull == 1:
+				linha1 = linha1 + " && cd %s/%s && sh git_upl.sh"%(homepath,gitpath)
 			print >> file, linha
 
-		print >> file, ""
-
+		#print >> file, ""
 		file.close
+
+if threads != 1:
+	file = open("%s/%s/%s"%(homepath,shpath,filename),"r")
+	lines = file.readlines()
+	tam = len(lines)
+	for x in range(threads):
+		file2 = open("%s/%s/%d_%s"%(homepath,shpath,x+1,filename),"w")
+		i = x*4
+		while i < tam:
+			line = lines[i]
+			print >> file2, line
+			i = i+1
+			if ((i-1)%4) == 3:
+				i = i+threads*3
+	file2.close
+file.close

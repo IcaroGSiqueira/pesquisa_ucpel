d72e1e14aeb05843b0f93fb83f71f0e76b62708c
Ícaro <icaro.siqueira@sou.ucpel.edu.br>
2019-11-13 14:31:51 -0300

    commit

--- codes/run-sh/py/run_hm.py
+++ codes/run-sh/py/run_hm.py
@@ -1,46 +1,158 @@
 import os
 
-f=30
+f=200
 
 clip=0
-gprof=1
+gprof=0
 
-#yuvs = ["BasketballDrill_832x480_50.yuv","BasketballDrive_1920x1080_50.yuv","BasketballPass_416x240_50.yuv","Kimono_1920x1080_24.yuv","BlowingBubbles_416x240_50.yuv","ParkScene_1920x1080_24.yuv","BQMall_832x480_60.yuv","BQSquare_416x240_60.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","PartyScene_832x480_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","Traffic_2560x1600_30_crop.yuv","SlideEditing_1280x720_30.yuv","Tennis_1920x1080_24.yuv"] #SERVIDOR
+gitpull = 1
+
+server = 0 # local = 0 ; servidor = 1
+
+#[37,32,27,22]
+
+qps = [22,27,32,37]
+
+threads = 4 # numero de processos em parelelo
+
+gitpath = "pesquisa_ucpel"
+gitscript = "git_upl"
+
+shpath = "pesquisa_ucpel/codes/run-sh/hm"
+filename = "run_hm.sh"
+
+yuvs = ["BlowingBubbles_416x240_50.yuv","BasketballPass_416x240_50.yuv","BQSquare_416x240_60.yuv","RaceHorses_416x240_30.yuv","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","RaceHorses_832x480_30.yuv","PartyScene_832x480_50.yuv","BasketballDrive_1920x1080_50.yuv","Kimono_1920x1080_24.yuv","ParkScene_1920x1080_24.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","Tennis_1920x1080_24.yuv","SlideEditing_1280x720_30.yuv","FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","Traffic_2560x1600_30_crop.yuv"] #SERVIDOR
 
 #yuvs = ["BasketballDrill_832x480_50.yuv","BasketballDrive_1920x1080_50.yuv","BasketballPass_416x240_50.yuv","Kimono_1920x1080_24.yuv","BlowingBubbles_416x240_50.yuv","ParkScene_1920x1080_24.yuv","BQMall_832x480_60.yuv","BQSquare_416x240_60.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","PartyScene_832x480_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","RaceHorses_832x480_30.yuv","RaceHorses_416x240_30.yuv","Traffic_2560x1600_30_crop.yuv"] #UCPEL_PC
 
-yuvs = ["ParkScene_1920x1080_24.yuv"]
-
 #yuvs = os.listdir("%s"%yuvpath)
 
-homepath = "/home/icaro" #UCPEL
-#homepath = "/home/grellert" #SERVIDOR
-#yuvpath = "/videos" #SERVIDOR
-yuvpath = "/home/icaro/origCfP" #UCPEL
+if server == 1:	
+	homepath = "/home/grellert"
+	yuvpath = "/videos"
+	outpath = "pesquisa_ucpel/output_HM/server"
+	binpath = "hm/bin" #partindo da homepath
+	confpath = "hm/cfg"
 
-encpath = "HM-16.9-approx" #UCPEL
-#encpath = "hm-16.9_approx_ucpel" #SERVIDOR
-binout = "testesHEVC/gpbin"
-out = "testesHEVC/gpout"
+	yuvs = ["BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","PartyScene_832x480_50.yuv","FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","SlideEditing_1280x720_30.yuv","Tennis_1920x1080_24.yuv","ParkScene_1920x1080_24.yuv","BasketballDrive_1920x1080_50.yuv","Kimono_1920x1080_24.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","Traffic_2560x1600_30_crop.yuv"]
 
-file = open("%s/pesquisa_ucpel/codes/run-sh/hm/run_hm.sh"%homepath,"w")
+else:
+	homepath = "/home/icaro"
+	yuvpath = "/home/icaro/origCfP"
+	outpath = "pesquisa_ucpel/output_HM/local"
+	binpath = "hm/bin" #partindo da homepath
+	confpath = "hm/cfg"
 
-for taps in [8]:#,6,4,2]:
+	#yuvs = ["BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30.yuv","RaceHorses_832x480_30.yuv","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","PartyScene_832x480_50.yuv","ParkScene_1920x1080_24.yuv","BasketballDrive_1920x1080_50.yuv","Kimono_1920x1080_24.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","PeopleOnStreet_2560x1600_30_crop.yuv","Traffic_2560x1600_30_crop.yuv"]
+
+if gprof == 0:
+	bina = "TAppEncoderStatic"
+else:
+	bina = "TAppEncoderStaticd"
+	inf = "gprof"
+
+file = open("%s/%s/%s"%(homepath,shpath,filename),"w")
+
+try:
+	os.system("mkdir %s/%s/bin"%(homepath,outpath))
+except:
+	pass
+try:
+	os.system("mkdir %s/%s/out"%(homepath,outpath))
+except:
+	pass
+
+if gprof == 1:
+	try:
+		os.system("mkdir %s/%s/gprof"%(homepath,outpath))
+	except:
+		pass
+	try:
+		os.system("mkdir %s/%s/gmon"%(homepath,outpath))
+	except:
+		pass
+
+for taps in [8,6,4,2]:
 	for yuv in yuvs:
-		for qp in [37,32,27,22]:
+		for qp in qps:
+
 			if ".py" in yuv:
 				continue
 			if "bit" in yuv:
 				continue
+
 			if "crop" in yuv:
 				vid,pix,fr,y = yuv.split("_")
-				nome = vid+"_"+pix+"_"+fr+"_qp%s_"%(qp)+y
+				nome = vid+"_"+pix+"_"+fr
 			else:
 				vid,pix,fps = yuv.split("_")
 				fr,y = fps.split(".")
-				nome = vid+"_"+pix+"_"+fr+"_%dqp_"%(qp)+y
+				nome = vid+"_"+pix+"_"+fr
+
 			w,h = pix.split("x")
+
+			if conf == "encoder_randomaccess_main.cfg":
+				info = "%staps_%sqp_%sfframes_RA_"%(taps,qp,f) + inf
+			elif conf == "encoder_lowdelay_main.cfg":
+				info = "%staps_%sqp_%sfframes_LD_"%(taps,qp,f) + inf
+			elif conf == "encoder_intra_main.cfg":
+				info = "%staps_%sqp_%sfframes_IO_"%(taps,qp,f) + inf
+			else:
+				info = "%staps_%sqp_%sfframes_"%(taps,qp,f) + inf
+
 			#print w,h,f,fr,qp
+
+			if gprof == 1:
+				linha = "%s/%s/%s -c %s/%s/%s --InputFile=\"%s/%s\" -fr %s --SourceWidth=%s --SourceHeight=%s -q %s -f %s --fme_filter_ntaps=%s -b 8 %s --BitstreamFile=\"%s/%s/bin/%s_%s.bin\" > %s/%s/out/%s_%s.txt"%(homepath,binpath,bina,homepath,confpath,conf,yuvpath,yuv,fr,w,h,qp,f,taps,simd,homepath,outpath,nome,info,homepath,outpath,nome,info)
+
+				linha2 = "mv %s/%s/gmon.out %s/%s/gmon/gmon_%s_%s.out"%(homepath,shpath,homepath,outpath,nome,info)
+
+				linha3 = "gprof %s/%s/%s %s/%s/gmon/gmon_%s_%s.out > %s/%s/gprof/%s_%s.txt"%(homepath,binpath,bina,homepath,outpath,nome,info,homepath,outpath,nome,info)
+
+				linha4 =  "echo \"%s_%s DONE!\""%(nome,info)
+
+				#VERIFICAR SOBRESCRICAO
+				try:
+					test = open("%s/%s/out/%s_%s.txt"%(homepath,outpath,nome,info),"r")
+					tlines = test.readlines()
+					tline = tlines[-1]
+					if "Total Time" in tline:
+						if gitpull == 1:
+							linhag = "cd %s/%s && sh %s.sh"%(homepath,gitpath,gitscript)
+							print >> file, linha + " && " + linha2 + " && " + linha3 + " && " + linha4 + " && " + linhag
+						else:
+							print >> file, linha + " && " + linha2 + " && " + linha3 + " && " + linha4
+				except:
+					if gitpull == 1:
+						linhag = "cd %s/%s && sh %s.sh"%(homepath,gitpath,gitscript)
+						print >> file, linha + " && " + linha2 + " && " + linha3 + " && " + linha4 + " && " + linhag
+					else:
+						print >> file, linha + " && " + linha2 + " && " + linha3 + " && " + linha4
+				
+			else:
+				linha = "%s/%s/%s -c %s/%s/%s --InputFile=\"%s/%s\" -fr %s --SourceWidth=%s --SourceHeight=%s -q %s -f %s --fme_filter_ntaps=%s -b 8 %s --BitstreamFile=\"%s/%s/bin/%s_%s.bin\" > %s/%s/out/%s_%s.txt"%(homepath,binpath,bina,homepath,confpath,conf,yuvpath,yuv,fr,w,h,qp,f,taps,simd,homepath,outpath,nome,info,homepath,outpath,nome,info)
+				
+				linha4 =  "echo \"%s_%s DONE!\""%(nome,info)
+
+				try:
+					test = open("%s/%s/out/%s_%s.txt"%(homepath,outpath,nome,info),"r")
+					tlines = test.readlines()
+					tline = tlines[-1]
+					if "Total Time" in tline:
+						if gitpull == 1:
+							linhag = "cd %s/%s && sh %s.sh"%(homepath,gitpath,gitscript)
+							print >> file, linha + " && " + linha4 + " && " + linhag
+						else:
+							print >> file, linha + " && " + linha4
+				except:
+					if gitpull == 1:
+						linhag = "cd %s/%s && sh %s.sh"%(homepath,gitpath,gitscript)
+						print >> file, linha + " && " + linha4 + " && " + linhag
+					else:
+						print >> file, linha + " && " + linha4
+
+
+
 			if gprof == 0:
 				if taps == 8:
 					linha = "{ time %s/%s/bin/TAppEncoderStatic -c %s/%s/cfg/encoder_randomaccess_main.cfg --InputFile=%s/%s --SourceHeight=%s --SourceWidth=%s -f %s -fr %s -q %s --fme_filter_ntaps=%s --BitstreamFile=%s/%s/%s_%df.bin ; } &> %s/%s/%s_%dqp_%df_hm_out" %(homepath,encpath,homepath,encpath,yuvpath,yuv,h,w,f,fr,qp,taps,homepath,binout,nome,f,homepath,out,yuv,qp,f)
